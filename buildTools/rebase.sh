9ca64222874a59db9a0e072105d98e6dfcd390a9
dangherve <dangherve@gmail.com>
2026-03-07 01:18:22 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/ewanmellor/git-diff-image.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/git-diff-image.git
+git push --force --set-upstream origin master
